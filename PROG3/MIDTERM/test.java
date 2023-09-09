d0247c6dd1df770cc6fde3119ebb8e1e7c4e7727
Numinous <seuriin@gmail.com>
2023-09-09 13:01:19 +0800

    Create test.java
    I friggin' know Java (basics), I'm just compiling...

---
+++ PROG3/MIDTERM/test.java
@@ -0,0 +1,20 @@
+package test;
+
+public class test
+{
+    public static void main(String[]args)
+    {
+        double a = 5;
+        double b = 20;
+        
+        double sum = a + b;
+        double diff = a - b;
+        double prod = a * b;
+        double quo = a / b;
+        
+        System.out.println("Sum: "+ sum);
+        System.out.println("Diff: "+ diff);
+        System.out.println("Prod: "+ prod);
+        System.out.println("Quo: "+ quo);
+    }
+}
